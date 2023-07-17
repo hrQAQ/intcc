faabb2477bbb0dcda3a1ef04d0497ddc1efd8365
horik <hr.mail.qaq@gmail.com>
2023-07-17 14:23:36 +0800

    patch: modify env.sh

--- env.sh
+++ env.sh
@@ -1,2 +1,10 @@
+#!/bin/zsh
 conda activate py27
-CXXFLAGS="-Wno-error -std=c++11" CC='gcc-5' CXX='g++-5' ./waf configure
+if [ "$1" == "debug" ]; then
+    echo "debug"
+    CXXFLAGS="-Wno-error -std=c++11" CC='gcc-5' CXX='g++-5' ./waf configure --build-profile=debug
+elif [ "$1" == "opt" ]; then
+    echo "optimized"
+    CXXFLAGS="-Wno-error -std=c++11" CC='gcc-5' CXX='g++-5' ./waf configure --build-profile=optimized
+fi
+
